c472cb24fe45625431f56a0bdc4b140e973ce97c
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-09 14:45:10 -0500

    Created TIY7 files.

--- do_for_me.py
+++ do_for_me.py
@@ -1,8 +1,8 @@
 import os
 for filename in os.listdir("/Users/pass512cs15/PycharmProjects/Chapter14Anthis"):
-    if "TIY1" in filename:
+    if "TIY6" in filename:
         with open(filename) as f2:
             content = f2.readlines()
-        with open(f"TIY6{filename[4:]}", "w+") as f:
-            print(f"Created {f}")
+        with open(f"TIY7{filename[4:]}", "w+") as f:
+            print(f"Created {f.name} in encoding type {f.encoding}.")
             f.writelines(content)
